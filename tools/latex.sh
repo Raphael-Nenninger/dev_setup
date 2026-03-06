b357ecf2c5669f22c833eea165e615d5103b6640
Raphael <raphael@dev.com>
2026-03-06 16:11:42 +0100

    Tools installiert scripts fertig

---
+++ tools/latex.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eou pipfail
+
+# Latex Setup (Tex Lilve + Extras + Texmaker) für Ubuntu
+
+echo "===> apt update"
+sudo apt update
+
+echo "===> LaTeX Pakete installieren"
+sudo apt-get install -y \
+  texlive \
+  texlive-bibtex-extra \
+  texlive-extra-utils \
+  texlive-humanities \
+  texlive-lang-german \
+  texlive-publishers \
+  texmaker
+sudo apt install zathura
+echo "==> Verify"
+pdflatex --version | head -n 1
+bibtex --version | head -n 1
+texmaker --version || true
+
+echo "==> Done."
