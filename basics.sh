88dbaf2a6b44a756c067c25ba934d8a92fd704bc
Raphael <raphael@dev.com>
2026-03-07 18:17:23 +0100

    Dev Script done

--- basics.sh
+++ basics.sh
@@ -1,5 +1,29 @@
-#!/usr/bin/sh
-#
-# Script to install the basic utils
-#
+#!/usr/bin/env bash
+set -eou pipefail
+
+echo "==> Basic Programms for installing"
+sudo apt update
 sudo apt install git curl fzf 
+
+echo "==> Führe Dateien in programming aus"
+for file in programming/*; do
+	if [[ -f "$file" && -x "$file" ]]; then 
+		echo "Führe aus: $file"
+		"$file"
+	fi
+done
+
+
+echo "==> Führe Dateien in tools aus"
+for file in tools/*; do
+	if [[ -f "$file" && -x "$file" ]]; then 
+		echo "Führe aus: $file"
+		"$file"
+	fi
+done
+
+echo "==> Dateien aus env nach ~/.config kopieren"
+mkdir -p ~/.config
+cp -r env/* ~/.config
+
+echo "==> Fertig"
