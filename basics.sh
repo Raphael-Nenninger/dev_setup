8ff51357d20ed74143e3cf5d23dc3a7e018ae70e
Raphael <raphael@dev.com>
2026-03-08 12:25:27 +0100

    basics hinzugefügt

--- basics.sh
+++ basics.sh
@@ -5,7 +5,7 @@ echo "==> Basic Programms for installing"
 sudo apt update
 # Nach frischer Installation auch das System updaten
 sudo apt upgrade -y
-sudo apt install git curl fzf 
+sudo apt install git curl fzf thunar
 
 echo "==> Führe Dateien in programming aus"
 for file in programming/*; do
